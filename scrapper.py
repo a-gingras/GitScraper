0ed0c597caa8777e6f7e9f46c593c8cd03d08f77
Daniel <dan.laframb@gmail.com>
2020-05-05 20:22:13 -0400

    Getting all repos from first page. Figured out pagination pattern. Probably try to move to next page and scan package.jsons next

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,20 @@
 # Imports
 from bs4 import BeautifulSoup
 import requests
+import re
+
+# Global for all found repo urls
+repo_urls = []
+
+# Pagination starts at 2 for the query
+page = 2
+
+# First half of query url
+first_half_url = 'https://github.com/search?o=desc&p='
+
+# Second half of query url
+second_half_url = '&q=react+native&s=updated&type=Repositories'
+
 
 # Hardcoded URL for now.
 # Query for react native repose that were most recenty updated.
@@ -13,4 +27,22 @@ page_response = requests.get(page_link, timeout=5)
 page_content = BeautifulSoup(page_response.content, "html.parser")
 
 for link in page_content.find_all('a'):
-        print(link.get('href'))
+    reg = '^/\w+(?:-\w+)+/\w+(?:-\w+)+$'
+    result = re.match(reg, link.get('href'))
+    if(result):
+       repo_urls.append(link.get('href'))
+
+for link in page_content.find_all('a'):
+    reg = '^/(?!topics)(?!features)\w+/\w+$'
+    result = re.match(reg, link.get('href'))
+    if(result):
+        repo_urls.append(link.get('href'))
+
+for link in page_content.find_all('a'):
+    reg = '^/(?!topics)\w+/\w+(?:-\w+)+$'
+    result = re.match(reg, link.get('href'))
+    if(result):
+        repo_urls.append(link.get('href'))
+
+
+print(repo_urls)
