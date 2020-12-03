8592f99337a0836ba85d96bf2ec46df8a3a82d0c
Anne Gingras <anne.gingras@youi.tv>
2020-12-03 18:28:31 -0500

    lots of changes

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-README file for GitScraper
+# GitScrapper
+
+This is a script that looks through various React Native projects on Github to see which props are the most used. 
+
+## Installation
+Install the following python libraries:
+- BeautifulSoup
+- PyGithub
+
+Make a file called `tokens.json` in the following format with your Github tokens:
+```json
+{
+    "tokens": [
+        "token1",
+        "token2"
+        ...
+    ]
+}
+```
+It's highly recommended to get multiple tokens because Github's rate limit is very low and this will run very slowly and error out often with only one token. 
+
+~~To get more tokens you can ask your friends for theirs :).~~
+
+## Run
+
+Run the following where <COMPONENT> is the name of the component you want to look at:
+```bash
+python3 scrapper.py <COMPONENT> new
+```
+Running the script will go through up to 100 repos. Run without "new" to continue where it left off and look are more repos. 
+
+## Troubleshooting
+If the script stops with an `"API rate limit exceeded for user ID ___."` error just wait a minute and run again without "new" to continue.
+
+## Getting Results
+Go to `output/` to get a json file with the results.
--- scrapper.py
+++ scrapper.py
@@ -5,70 +5,37 @@ import re
 import time
 import json
 import operator
+import sys
+from github import Github
 
-# Global for all found repo urls
+COMPONENT_NAME = sys.argv[1]
+
+token_idx = 0
+with open("tokens.json") as json_file:
+    tokens = json.load(json_file)["tokens"]
+
+g = Github(tokens[token_idx])
 repo_urls = []
-# Github base url
+props_list = []
+
 base_url = 'https://github.com'
+data_filename = "output/" + COMPONENT_NAME + ".json"
+repos_filename = "output/stuff/" + COMPONENT_NAME + "Repos.txt"
 
-COMPONENT_NAME = 'ScrollView'
 
-props_list = [
-'alwaysBounceHorizontal',
-'alwaysBounceVertical',
-'automaticallyAdjustContentInsets',
-'bounces',
-'bouncesZoom',
-'canCancelContentTouches',
-'centerContent',
-'contentContainerStyle',
-'contentInset',
-'contentInsetAdjustmentBehavior',
-'contentOffset',
-'decelerationRate',
-'directionalLockEnabled',
-'disableIntervalMomentum',
-'disableScrollViewPanResponder',
-'endFillColor',
-'horizontal',
-'indicatorStyle',
-'invertStickyHeaders',
-'keyboardDismissMode',
-'keyboardShouldPersistTaps',
-'maintainVisibleContentPosition',
-'maximumZoomScale',
-'minimumZoomScale',
-'nestedScrollEnabled',
-'onContentSizeChange',
-'onMomentumScrollBegin',
-'onMomentumScrollEnd',
-'onScroll',
-'onScrollBeginDrag',
-'onScrollEndDrag',
-'onScrollToTop',
-'overScrollMode',
-'pagingEnabled',
-'persistentScrollbar',
-'pinchGestureEnabled',
-'refreshControl',
-'removeClippedSubviews',
-'scrollBarThumbImage',
-'scrollEnabled',
-'scrollEventThrottle',
-'scrollIndicatorInsets',
-'scrollPerfTag',
-'scrollToOverflowEnabled',
-'scrollsToTop',
-'showsHorizontalScrollIndicator',
-'showsVerticalScrollIndicator',
-'snapToAlignment',
-'snapToEnd',
-'snapToInterval',
-'snapToOffsets',
-'snapToStart',
-'stickyHeaderIndices',
-'zoomScale'
-]
+def get_props_list():
+    props = []
+    docs_url = "https://reactnative.dev/docs/0.60/" + COMPONENT_NAME.lower()
+    docs_page_response = requests.get(docs_url, timeout=5)
+    docs_page_content = BeautifulSoup(docs_page_response.content, "html.parser")
+
+    props_a =  docs_page_content.find('a', text="Props")
+    for child in props_a.next_sibling:
+        prop_a = child.find('a')
+        if "DEPRECATED" not in prop_a.text:
+            props.append(prop_a.text)
+            print(prop_a.text)
+    return props
 
 def get_repos(link):
     page_response = requests.get(link, timeout=5)
@@ -81,18 +48,19 @@ def get_repos(link):
 
     next_button = page_content.find('a', string='Next')
     if next_button and len(repo_urls) < 100: # keep going until 100 new repos are found
-        time.sleep(1)
         get_repos(next_button.get('href'))
 
-def get_files(url):
-    time.sleep(2)
-    page_response = requests.get(url, timeout=5)
-    page_content = BeautifulSoup(page_response.content, "html.parser")
-    for file_name in page_content.find_all('a', string=re.compile('.*\.(tsx)|.*\.(js)$')):
-        file_urls.append(file_name.get('href'))
-    next_button = page_content.find('a', string='Next')
-    if next_button:
-        get_files(base_url + next_button.get('href'))   
+def get_files(repo_name): 
+    files = []
+    query_js = "repo:" + repo_name[1:] + " " + COMPONENT_NAME + " extension:js"
+    results_js = g.search_code(query_js, order='desc')
+    query_tsx = "repo:" + repo_name[1:] + " " + COMPONENT_NAME + " extension:tsx"
+    results_tsx = g.search_code(query_tsx, order='desc')
+    for result in results_js:
+        files.append(result)
+    for result in results_tsx:
+        files.append(result)
+    return files
 
 def find_closing_bracket(start, code):
     current = start + 1
@@ -113,6 +81,7 @@ def find_closing_bracket(start, code):
         current = next_ + 1      
 
 def parse_js_code(code):
+    code = str(code)
     current = 0
     while code.find('<' + COMPONENT_NAME, current) != -1:
         start = code.find('<' + COMPONENT_NAME, current)
@@ -131,51 +100,62 @@ def parse_js_code(code):
                 props[prop] = props[prop] + 1
         current = end        
 
+
+props_list = get_props_list()
 # get all substring props
 substring_props = []
 for p in props_list:
     for pp in props_list:
         if p in pp and p != pp:
             substring_props.append(p)  
-print(substring_props)    
+print("substring props: ", substring_props)   
 
-# make new props object
-# props = {}
-# for p in props_list:
-#     props[p] = 0
+if len(sys.argv) > 2 and sys.argv[2] == "new":
+    # make new props object
+    props = {}
+    for p in props_list:
+        props[p] = 0
+    # clear repos    
+    open(repos_filename, 'w').close()
+    old_repos = []    
+else:
+    # get previously stored data
+    with open(data_filename) as json_file:
+        props = json.load(json_file)
 
-# get previously stored data
-with open('data.json') as json_file:
-    props = json.load(json_file)
+    # get list of repos that have already been looked at
+    with open(repos_filename, 'r') as f:
+        old_repos = f.readlines()
+        old_repos = [r[:-1] for r in old_repos]    
 
-# get list of repos that have already been looked at
-with open('repos.txt', 'r') as f:
-    old_repos = f.readlines()
-    old_repos = [r[:-1] for r in old_repos]
-
+# get list of repos that depend on react-native-video
 page_link = 'https://github.com/react-native-community/react-native-video/network/dependents?package_id=UGFja2FnZS0xNDgwNzUwNw%3D%3D'
 get_repos(page_link) 
 
 for url in repo_urls:
-    file_urls = []
+    # check the rate limit and wait if the limit has been passed
+    rate_limit = g.get_rate_limit()
+    while rate_limit.search.remaining <= 5 or rate_limit.core.remaining < 400:
+        token_idx = (token_idx + 1) % len(tokens)
+        print("Rate limit remaining:", rate_limit.search.remaining, rate_limit.core.remaining, "token_idx:", token_idx)
+        g = Github(tokens[token_idx])
+        print("\nWaiting for rate limit to reset...")
+        time.sleep(2)
+        rate_limit = g.get_rate_limit()    
+
     print('\nREPO: ', url)
-    # search repo for files containing 'video'
-    get_files(base_url + url + '/search?q=' + COMPONENT_NAME)
-    for file_name in file_urls:
-        print('JS/TSX FILE:', file_name)
-        time.sleep(1)
-        page_response = requests.get(base_url + file_name , timeout=5)
-        page_content = BeautifulSoup(page_response.content, "html.parser")
-        code_body = page_content.find_all('table')
-        if code_body and ('<' + COMPONENT_NAME) in code_body[0].text:
-            parse_js_code(code_body[0].text)
-            a_file = open("data.json", "w")
-            json.dump(props, a_file)
-            a_file.close()
-    with open("repos.txt", "a") as myfile:
+    # get all js/tsx files that have the component name in them
+    files = get_files(url)   
+    for file_ in files:
+        print("File:", file_.name)
+        # parse the file for props
+        parse_js_code(file_.decoded_content)
+
+    # update data.json and repos list
+    a_file = open(data_filename, "w")
+    json.dump(props, a_file)
+    a_file.close()
+    with open(repos_filename, "a") as myfile:
         myfile.write(url + "\n")
 
-# sort data and print
-sorted_props = sorted(props.items(), key=operator.itemgetter(1), reverse=True)
-props = dict(sorted_props)
-print(props)
+
